9b6b64fb3f4975b1a7c933aa38ec31abeee8db4a
Fabián <fabian.arancibiab@sansano.usm.cl>
2021-10-22 10:55:52 -0300

    still testing

--- server/Lider.go
+++ server/Lider.go
@@ -30,8 +30,7 @@ func (s *server) SendPlays(ctx context.Context, in *pb.SendRequest) (*pb.SendRep
 func main() {
 
 
-	/*// nos convertimos en servidor (LIDER)
->>>>>>> 46caeead2c7336ca9916db6c3ef496d7e5fcba80
+	// nos convertimos en servidor (LIDER)
 	listner, err := net.Listen("tcp", ":8080")
 
 	if err != nil {
@@ -44,7 +43,7 @@ func main() {
 	if err = serv.Serve(listner); err != nil {
 		log.Printf("paso por el fallo")
 		panic("cannot initialize the server" + err.Error())
-	}*/
+	}
 	/////////////
 
 	var first string
