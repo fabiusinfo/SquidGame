5eaa02aa806fb5d3177c1bb1014a58c6daf57db3
Fabián <fabian.arancibiab@sansano.usm.cl>
2021-10-29 16:15:15 -0300

    gorutina

--- client/pozo.go
+++ client/pozo.go
@@ -38,7 +38,7 @@ func failOnError(err error, msg string) {
 
 func main() {
 
-	/*go func(){
+	go func(){
 	listner, err := net.Listen("tcp", ":8080")
 	//conn, err := grpc.Dial("10.6.43.41:8080", grpc.WithInsecure())
 
@@ -109,44 +109,6 @@ func main() {
 	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever*/
-	conn, err := amqp.Dial("amqp://admin:test@10.6.43.41:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	forever := make(chan bool)
-
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-		}
-	}()
-
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
+
 }
