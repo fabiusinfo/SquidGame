2849de9312f4c5e9149f5507127324f18cac6dd2
INFABIUS <fabian.arancibiab@sansano.usm.cl>
2021-10-29 15:34:00 -0300

    Update pozo.go

--- client/pozo.go
+++ client/pozo.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"context"
-	//"fmt"
-	//"io/ioutil"
+	"fmt"
+	"io/ioutil"
 	"log"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 
 	//"fmt"
 	pb "github.com/fabiusinfo/SquidGame/proto"
-	//"google.golang.org/grpc"
+	"google.golang.org/grpc"
 
 	//"math/rand"
 	//"strconv"
 	//"time"
-	//"log"
-	//"net"
+	"log"
+	"net"
 )
 
 type server struct {
@@ -38,7 +38,7 @@ func failOnError(err error, msg string) {
 
 func main() {
 
-	/*go func(){
+	go func(){
 	listner, err := net.Listen("tcp", ":8080")
 	//conn, err := grpc.Dial("10.6.43.41:8080", grpc.WithInsecure())
 
@@ -52,7 +52,7 @@ func main() {
 		panic("cannot initialize the server" + err.Error())
 
 	}
-	}()*/
+	}()
 
 	conn, err := amqp.Dial("amqp://admin:test@10.6.43.41:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
