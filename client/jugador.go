446804fa7001504e64f944c2295801469fdaa127
jiolivar <javier.olivares.13@sansano.usm.cl>
2021-10-30 16:21:15 -0300

    a

--- client/jugador.go
+++ client/jugador.go
@@ -3,29 +3,30 @@ package main
 import (
 	"context"
 	"fmt"
+
 	pb "github.com/fabiusinfo/SquidGame/proto"
 	"google.golang.org/grpc"
 
 	//"math/rand"
-	"strconv"
 	"log"
-	"time"
 	"math/rand"
+	"strconv"
+	"time"
 )
 
 func main() {
 	var action string
 	playerNumber := "15"
 	play := "2"
-	actualStage:="none"
+	actualStage := "none"
 	codes1 := "none"
 	codes2 := "none"
 	codes3 := "none"
 	alive := true
-	started:=false
-	var playersAlive[16]bool
+	started := false
+	var playersAlive [16]bool
 	//inscribimos los bots
-	for i:=0 ; i<15 ; i++ {
+	for i := 0; i < 15; i++ {
 		conn, err := grpc.Dial("10.6.43.41:8080", grpc.WithInsecure())
 
 		if err != nil {
@@ -42,19 +43,19 @@ func main() {
 		}
 		log.Printf("inscrito")
 		//signed=r.GetSigned()
-		playersAlive[i]=true
+		playersAlive[i] = true
 		//codes1 = r.GetCodes1()
 		codes2 = r.GetCodes2()
 		//codes3 = r.GetCodes3()
 		//actualStage=codes1
 		fmt.Println("inscripción al SquidGame realizada con éxito.")
 	}
-	fmt.Println(codes1+codes2+codes3)
+	fmt.Println(codes1 + codes2 + codes3)
 	for alive {
 		//fmt.Println("ID del jugador: " + playerNumber + " , Jugada: " + play + " , etapa: " + actualStage)
 		fmt.Println("Activar jugador, join->unirse, send->enviar jugadas, amount->solicitar monto: ")
 		fmt.Scanln(&action)
-		if action == "send"{
+		if action == "send" {
 			fmt.Println("realiza jugada: ")
 			fmt.Scanln(&play)
 		}
@@ -70,8 +71,8 @@ func main() {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
 
-		switch action{
-			// unirse al juego del calamar
+		switch action {
+		// unirse al juego del calamar
 		case "join":
 			if actualStage == "none" {
 				r, err := servicePlayer.JoinGame(ctx, &pb.JoinRequest{Player: playerNumber})
@@ -83,7 +84,7 @@ func main() {
 				codes1 = r.GetCodes1()
 				codes2 = r.GetCodes2()
 				codes3 = r.GetCodes3()
-				actualStage=codes1
+				actualStage = codes1
 				started = true
 				fmt.Println("inscripción al SquidGame realizada con éxito.")
 			} else {
@@ -91,74 +92,73 @@ func main() {
 			}
 			//enviar jugada realizada
 		case "send":
-			if actualStage != "none" && started == true{
-				
-		
+			if actualStage != "none" && started == true {
+
 				r, err := servicePlayer.SendPlays(ctx, &pb.SendRequest{Player: playerNumber, Play: play, Stage: actualStage})
 				if err != nil {
 					log.Fatalf("fallo 1: %v", err)
 				}
-				if err2 != nil {
-					log.Fatalf("fallo 2: %v", err2)
-				}
+				/*
+					if err2 != nil {
+						log.Fatalf("fallo 2: %v", err2)
+					} */
+
 				//log.Printf("Greeting: %s", r.GetMessage())
-				actualStage=r.GetStage()
+				actualStage = r.GetStage()
 				alive = r.GetAlive()
 				//started = r.GetStarted()
 
 				//Este pedazo de código es para las jugadas de los bots
 
-				for i:=0 ; i<15 ; i++ {
-					if playersAlive[i]==true{
+				for i := 0; i < 15; i++ {
+					if playersAlive[i] == true {
 						fmt.Println(strconv.Itoa(i))
-						botPlayer:=strconv.Itoa(i)
-					conn, err := grpc.Dial("10.6.43.41:8080", grpc.WithInsecure())
+						botPlayer := strconv.Itoa(i)
+						conn, err := grpc.Dial("10.6.43.41:8080", grpc.WithInsecure())
 
-					if err != nil {
-						panic("cannot connect with server " + err.Error())
-					}
+						if err != nil {
+							panic("cannot connect with server " + err.Error())
+						}
 
-					servicePlayer := pb.NewSquidGameServiceClient(conn)
+						servicePlayer := pb.NewSquidGameServiceClient(conn)
 
-					ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-					defer cancel()
+						ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+						defer cancel()
 
-					//play, err2 := strconv.Atoi(play)
-					//playsend:=int32(play)
-					//jugada aleatoria	
-					rand.Seed(time.Now().UnixNano())
-        				playsend:= rand.Int63n(10)+1
-						playsend= strconv.Itoa(playsend)
+						//play, err2 := strconv.Atoi(play)
+						//playsend:=int32(play)
+						//jugada aleatoria
+						rand.Seed(time.Now().UnixNano())
+						playsend := rand.Int63n(10) + 1
+						playsend = strconv.Itoa(playsend)
 						r, err := servicePlayer.SendPlays(ctx, &pb.SendRequest{Player: botPlayer, Play: playsend, Stage: actualStage})
-					if err != nil {
-						log.Fatalf("fallo 1: %v", err)
+						if err != nil {
+							log.Fatalf("fallo 1: %v", err)
+						}
+						//if err2 != nil {
+						//	log.Fatalf("fallo 2: %v", err2)
+						//}
+						//log.Printf("Greeting: %s", r.GetMessage())
+						actualStage = r.GetStage()
+						playersAlive[i] = r.GetAlive()
+						//started = r.GetStarted()
 					}
-					//if err2 != nil {
-					//	log.Fatalf("fallo 2: %v", err2)
-					//}
-					//log.Printf("Greeting: %s", r.GetMessage())
-					actualStage=r.GetStage()
-					playersAlive[i] = r.GetAlive()
-					//started = r.GetStarted()
-					}	
-							
+
 				}
 
-
-
-		} else{
-			fmt.Println("todavía no comienza el SquidGame o aún no estás inscrito.")
-		}
+			} else {
+				fmt.Println("todavía no comienza el SquidGame o aún no estás inscrito.")
+			}
 
 			//solicitar monto
 		case "amount":
-			message:= "solicito monto"
+			message := "solicito monto"
 			r, err := servicePlayer.AmountCheck(ctx, &pb.AmountRequest{Message: message})
 			if err != nil {
 				log.Fatalf("no se pudo solicitar el monto: %v", err)
 			}
 			log.Printf("Greeting: %s", r.GetMonto())
-		
+
 		default:
 			fmt.Println("ingresaste un mal comando.")
 
