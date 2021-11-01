9d885ac94d596bea51f5b6297f74a709b96bf162
Fabián <fabian.arancibiab@sansano.usm.cl>
2021-11-01 18:04:46 -0300

    base lvl 2

--- server/Lider.go
+++ server/Lider.go
@@ -93,51 +93,56 @@ func (s *server) SendPlays(ctx context.Context, in *pb.SendRequest) (*pb.SendRep
 					}
 				}
 
-				if pPlay > liderPlay {
-					alive = false
-					for i := 0; i < 16; i++ {
-						if list_of_players[i].id == in.GetPlayer() {
-							list_of_players[i].alive = false
+				if actualStage =="1rv" {
+
+					if pPlay > liderPlay {
+						alive = false
+						for i := 0; i < 16; i++ {
+							if list_of_players[i].id == in.GetPlayer() {
+								list_of_players[i].alive = false
+							}
 						}
+						conn, err := amqp.Dial("amqp://admin:test@10.6.43.41:5672/")
+						failOnError(err, "Failed to connect to RabbitMQ")
+						defer conn.Close()
+
+						ch, err := conn.Channel()
+						failOnError(err, "Failed to open a channel")
+						defer ch.Close()
+
+						q, err := ch.QueueDeclare(
+							"hello", // name
+							false,   // durable
+							false,   // delete when unused
+							false,   // exclusive
+							false,   // no-wait
+							nil,     // arguments
+						)
+						failOnError(err, "Failed to declare a queue")
+
+						i := in.GetPlayer()
+						s := in.GetStage()
+						//i_str := strconv.Itoa(int(i))
+
+						body := "Jugador_" + i + " Ronda_" + s
+
+						err = ch.Publish(
+							"",     // exchange
+							q.Name, // routing key
+							false,  // mandatory
+							false,  // immediate
+							amqp.Publishing{
+								ContentType: "text/plain",
+								Body:        []byte(body),
+							})
+						failOnError(err, "Failed to publish a message")
+						log.Printf(" ha muerdo: %d ", body)
+						//log.Printf(" [x] Sent %d ", body)
 					}
-					conn, err := amqp.Dial("amqp://admin:test@10.6.43.41:5672/")
-					failOnError(err, "Failed to connect to RabbitMQ")
-					defer conn.Close()
-
-					ch, err := conn.Channel()
-					failOnError(err, "Failed to open a channel")
-					defer ch.Close()
-
-					q, err := ch.QueueDeclare(
-						"hello", // name
-						false,   // durable
-						false,   // delete when unused
-						false,   // exclusive
-						false,   // no-wait
-						nil,     // arguments
-					)
-					failOnError(err, "Failed to declare a queue")
-
-					i := in.GetPlayer()
-					s := in.GetStage()
-					//i_str := strconv.Itoa(int(i))
-
-					body := "Jugador_" + i + " Ronda_" + s
-
-					err = ch.Publish(
-						"",     // exchange
-						q.Name, // routing key
-						false,  // mandatory
-						false,  // immediate
-						amqp.Publishing{
-							ContentType: "text/plain",
-							Body:        []byte(body),
-						})
-					failOnError(err, "Failed to publish a message")
-					log.Printf(" ha muerdo: %d ", body)
-					//log.Printf(" [x] Sent %d ", body)
-				}
-			} else {
+			} else if actualStage =="2tc" {
+				log.Printf("la jugada fue realizada en 2tc")
+			} 
+			}else {
 				log.Printf("aún no comienza el nivel")
 			}
 			//log.Printf("Greeting: %s", r.GetStage())
