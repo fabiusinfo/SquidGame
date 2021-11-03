cba5ce6fd3af558db0ea0204cab5243730bb7966
jiolivar <javier.olivares.13@sansano.usm.cl>
2021-11-03 16:05:32 -0300

    a

--- client/nameNode.go
+++ client/nameNode.go
@@ -114,7 +114,7 @@ func main() {
 			ip_maquina := linea[2]
 			//Ruta := "DN_plays/jugador_" + num_jugador + "__ronda_" + num_ronda + "rv.txt"
 			numerojugador := strings.Split(num_jugador, "_")
-			fmt.Println("El jugador: " + numerojugador + "tiene una jugada de la ronda: " + num_ronda + " en la ip: " + ip_maquina)
+			fmt.Println("El jugador: " + numerojugador[1] + "tiene una jugada de la ronda: " + num_ronda + " en la ip: " + ip_maquina)
 			s, e = Readln(r)
 		}
 	}
