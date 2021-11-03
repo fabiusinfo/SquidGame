ec7a9a349d8ce8ba01e07a8235d4a10c0bc2cbb3
jiolivar <javier.olivares.13@sansano.usm.cl>
2021-11-03 14:25:25 -0300

    me quiero coftar la pija

--- client/nameNode.go
+++ client/nameNode.go
@@ -111,7 +111,7 @@ func main() {
 			linea := strings.Split(s, " ")
 			//jugador := linea[0]
 			//numerojugador := strings.Split(jugador, "_")
-			fmt.Println(linea[2])
+			fmt.Println(linea)
 			s, e = Readln(r)
 		}
 	}
