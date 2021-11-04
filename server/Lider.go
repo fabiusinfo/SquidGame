77eb7d6934f01ffdb2be794d059d764417765863
KathiyS <katherine.salgado@sansano.usm.cl>
2021-11-04 18:20:07 -0300

    lo que sea

--- server/Lider.go
+++ server/Lider.go
@@ -280,7 +280,8 @@ func main() {
 			rand.Seed(time.Now().UnixNano())
 			fmt.Println("ronda " + strconv.Itoa(i+1))
 			liderPlay = int(rand.Int63n(5))
-			liderPlay = liderPlay + 6
+			liderPlay = 8
+			//liderPlay + 6
 
 			fmt.Println("jugada de lider: " + strconv.Itoa(liderPlay))
 			fmt.Println("escribe cualquier letra para la siguiente ronda: ")
